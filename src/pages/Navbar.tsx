b46e64a127a1e1147191e50ebe96ae4e90c466e6
OppositeWolf770 <shschool42@gmail.com>
2023-07-18 13:17:35 -0500

    Commit

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav className="topnav">
             <NavLink to={"/"}><img  className="ASNDlogo"src="/assets/ASND-logo.png" alt="ASND Media Logo" /></NavLink>
             <div className="topnav--links">
-                <NavLink to={"/about"}>About</NavLink>
+                <NavLink to={"/about"}>About Us</NavLink>
                 <NavLink to={"/packages"}>Boost Packages</NavLink>
                 <NavLink to={"/contact"}>Contact Us</NavLink>
                 <NavLink to={"/portfolio"}>Portfolio</NavLink>
